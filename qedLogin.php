52021b1cd0624b1be9a74a0aa6a4da220f4f05a9
Bhagyashree Limaye <Bhagyashree@Bhagyashrees-MacBook-Air.local>
2016-01-08 20:48:28 +0530

    Register done

--- qedLogin.php
+++ qedLogin.php
@@ -8,9 +8,10 @@
 			//echo $prn;
 			$pass=md5($_POST['pass']);
 			
-			$query="select id from ".$prefix."_User where email='$email' AND password='$pass'";
+			$query="select id from ".$prefix."_Users where email='$email' AND password='$pass'";
 			echo $query;
 			$result=mysqli_query($link,$query);
+			print_r($result);
 			$row=mysqli_fetch_row($result);
 			if(mysqli_num_rows($result)<1)
 				echo "Invalid Credentials";
