947619a772315cb723d94b944e7c4400028a95dd
Bhagyashree Limaye <Bhagyashree@Bhagyashrees-MacBook-Air.local>
2016-01-08 17:58:22 +0530

    added dbConnection file

---
+++ qedConnectDb.php
@@ -0,0 +1,16 @@
+<?php
+require("qeddbinfo.php");
+echo $host;
+$link=mysqli_connect($host,$user,$pass);
+if(!$link)
+{
+	die(mysqli_connect_error());
+}
+else
+	echo"successfully connected";
+
+if(!mysqli_select_db($link,$dbname))
+{
+	die(mysqli_connect_error());
+}
+?>
