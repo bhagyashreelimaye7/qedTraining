6eb8fc5e2752aca3b49fd6e4212b270714756d7d
Bhagyashree Limaye <Bhagyashree@Bhagyashrees-MacBook-Air.local>
2016-01-10 19:12:35 +0530

    Home and logout page

--- qedLogin.php
+++ qedLogin.php
@@ -9,9 +9,9 @@
 			$pass=md5($_POST['pass']);
 			
 			$query="select id from ".$prefix."_Users where email='$email' AND password='$pass'";
-			echo $query;
+			//echo $query;
 			$result=mysqli_query($link,$query);
-			print_r($result);
+			//print_r($result);
 			$row=mysqli_fetch_row($result);
 			if(mysqli_num_rows($result)<1)
 				echo "Invalid Credentials";
